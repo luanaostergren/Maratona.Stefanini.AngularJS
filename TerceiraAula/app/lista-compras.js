871529380697ec1666a057a79807f3ec2c30a593
luanaostergren <69064081+luanaostergren@users.noreply.github.com>
2020-10-14 17:57:28 -0300

    Add files via upload

---
+++ TerceiraAula/app/lista-compras.js
@@ -0,0 +1,22 @@
+(function (angular) {
+    "use strict";
+
+    // MODULO
+    angular.module('listaTarefasApp', []);
+
+    // CONTROLLER
+    angular.module('listaTarefasApp')
+        .controller('ListaTarefasController', listaTarefasController);
+
+    listaTarefasController.$inject = [];
+
+    function listaTarefasController() {
+        var vm = this;
+        // INIT VARIÁVEIS    
+        vm.logado = false;
+        vm.validar = false;
+        vm.register = false;
+
+    }
+
+})(angular);
